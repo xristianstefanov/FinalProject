fe13af511261052812f160772bbc96f924d671e0
Hristian Stefanov <xristian.stefanov10@gmail.com>
2020-11-12 15:11:46 +0200

    Views work

--- Web/LeagueOfLegendsChampions.Web.ViewModels/Champions/AddChampionInputModel.cs
+++ Web/LeagueOfLegendsChampions.Web.ViewModels/Champions/AddChampionInputModel.cs
@@ -1,6 +1,11 @@
 ﻿namespace LeagueOfLegendsChampions.Web.ViewModels.Champions
 {
+    using System.ComponentModel.DataAnnotations;
     public class AddChampionInputModel
     {
+        [Required]
+        [MinLength(30)]
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
