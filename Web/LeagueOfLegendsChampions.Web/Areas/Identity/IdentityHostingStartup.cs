605e954168569f91775baa16620309b8cb64b712
Hristian Stefanov <xristian.stefanov10@gmail.com>
2020-11-19 01:39:23 +0200

    Scaffold

---
+++ Web/LeagueOfLegendsChampions.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LeagueOfLegendsChampions.Data;
+using LeagueOfLegendsChampions.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LeagueOfLegendsChampions.Web.Areas.Identity.IdentityHostingStartup))]
+namespace LeagueOfLegendsChampions.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
